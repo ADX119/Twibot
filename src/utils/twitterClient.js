0f43c6f9b01b363609badb9569be27c30e9280e5
Aditya <atherva15@gmail.com>
2026-03-02 11:45:14 +0530

    fixed something

--- src/utils/twitterClient.js
+++ src/utils/twitterClient.js
@@ -2,11 +2,25 @@ import { TwitterApi } from "twitter-api-v2";
 import dotenv from "dotenv";
 dotenv.config();
 
+const requiredEnvVars = [
+  "API_KEY",
+  "API_SECRET",
+  "ACCESS_TOKEN",
+  "ACCESS_SECRET",
+];
+
+const missingEnvVars = requiredEnvVars.filter((key) => !process.env[key]);
+if (missingEnvVars.length > 0) {
+  throw new Error(
+    `Missing required Twitter env vars: ${missingEnvVars.join(", ")}`
+  );
+}
+
 export const client = new TwitterApi({
-  API_KEY: process.env.API_KEY,
-  API_SECRET: process.env.API_SECRET,
-  ACCESS_TOKEN: process.env.ACCESS_TOKEN,
-  ACCESS_SECRET: process.env.ACCESS_SECRET,
+  appKey: process.env.API_KEY,
+  appSecret: process.env.API_SECRET,
+  accessToken: process.env.ACCESS_TOKEN,
+  accessSecret: process.env.ACCESS_SECRET,
 });
 
 export const rwClient = client.readWrite;
